b1f74b9ece13f1be10df955946b60b3a480b791a
cxnt <pavelbartsits@gmail.com>
2021-10-01 16:06:18 +0300

    smarter frame converting

--- src/functions.py
+++ src/functions.py
@@ -5,16 +5,12 @@ import supervisely_lib as sly
 from supervisely_lib.io.fs import remove_dir
 
 
-def upload_and_reset(api: sly.Api, dataset_id, names, images, anns, metas, progress):
+def upload_frames(api: sly.Api, dataset_id, names, images_paths, anns, metas, progress):
     if len(names) > 0:
-        new_image_infos = api.image.upload_nps(dataset_id, names, images, metas=metas)
+        new_image_infos = api.image.upload_paths(dataset_id, names, images_paths, metas=metas)
         new_image_ids = [img_info.id for img_info in new_image_infos]
         api.annotation.upload_anns(new_image_ids, anns)
         progress.iters_done_report(len(names))
-    del names[:]
-    del images[:]
-    del anns[:]
-    del metas[:]
 
 
 def convert_tags(tags, prop_container, frame_container, frame_indices=None):
@@ -34,32 +30,39 @@ def add_object_id_tag(vobject_id, prop_container):
     prop_container.append(vobj_id_tag)
 
 
-def need_download(total_frames, total_annotated_frames):
-    frames_threshold = int(total_frames * g.label_threshold_percent)
+def need_download_video(total_frames, total_annotated_frames):
+    frames_threshold = int(total_frames * g.need_download_threshold)
     if total_annotated_frames > frames_threshold:
         return True
     return False
 
 
-def video_to_images(dataset_name, video_path, frames_to_convert): ## convert only len treshold frames?
+def get_frames_from_video(dataset_name, video_path, frames_to_convert): ## convert only len treshold frames?
+    image_names = []
     image_paths = []
     vidcap = cv2.VideoCapture(video_path)
-    success, image = vidcap.read()
-    count = 1
-    progress = sly.Progress("Converting frames to images", len(frames_to_convert))
-    while success:
-        if count - 1 in frames_to_convert:
-            image_name = dataset_name + "_" + str(count - 1).zfill(5) + ".png"
-            image_path = os.path.join(g.img_dir, image_name)
-            image_paths.append(image_path)
-            cv2.imwrite(image_path, image)
+    progress = sly.Progress("Extracting video frames", len(frames_to_convert))
+    for frame_number in frames_to_convert:
+        image_name = dataset_name + "_" + str(frame_number).zfill(5) + ".jpg"
+        image_names.append(image_name)
+        image_path = os.path.join(g.img_dir, image_name)
+        image_paths.append(image_path)
+        vidcap.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
+        success, image = vidcap.read()
+        cv2.imwrite(image_path, image)
+        progress.iter_done_report()
+    return image_names, image_paths
 
-            success, image = vidcap.read()
-            count += 1
-            progress.iter_done_report()
-        else:
-            success, image = vidcap.read()
-            count += 1
-            image_paths.append(None)
-    remove_dir(g.video_dir)
-    return image_paths
+
+def get_frames_from_api(api, video_id, dataset_name, frames_to_convert):
+    image_names = []
+    images_paths = []
+    progress = sly.Progress("Extracting frames video", len(frames_to_convert))
+    for frame_index in frames_to_convert:
+        image_name = dataset_name + "_" + str(frame_index).zfill(5) + ".jpg"
+        image_names.append(image_name)
+        image_path = os.path.join(g.img_dir, image_name)
+        api.video.frame.download_path(video_id, frame_index, image_path)
+        images_paths.append(image_path)
+        progress.iter_done_report()
+    return image_names, images_paths
--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 import os
-import cv2
 from collections import defaultdict
 import supervisely_lib as sly
 from supervisely_lib.video_annotation.key_id_map import KeyIdMap
@@ -22,14 +21,11 @@ def turn_into_images_project(api: sly.Api, task_id, context, state, app_logger):
         videos = api.video.get_list(dataset.id)
         for batch in sly.batched(videos):
             for video_info in batch:
-                name = sly.fs.get_file_name(video_info.name)
                 ann_info = api.video.annotation.download(video_info.id)
                 ann = sly.VideoAnnotation.from_json(ann_info, g.meta, key_id_map)
-
-                need_download = f.need_download(video_info.frames_count, len(ann.frames))
+                need_download_video = f.need_download_video(video_info.frames_count, len(ann.frames))
                 video_path = None
-                image_paths = None
-                if need_download is True or g.OPTIONS == "all":
+                if need_download_video is True or g.OPTIONS == "all":
                     video_path = os.path.join(g.video_dir, video_info.name)
                     api.video.download_path(video_info.id, video_path)
 
@@ -43,7 +39,6 @@ def turn_into_images_project(api: sly.Api, task_id, context, state, app_logger):
                     f.convert_tags(vobject.tags, object_props[vobject.key()], object_frame_tags[vobject.key()], frames_to_convert)
                     vobject_id = key_id_map.get_object_id(vobject.key())
                     f.add_object_id_tag(vobject_id, object_props[vobject.key()])
-
                 if g.OPTIONS == "annotated":
                     frames_to_convert.extend(list(ann.frames.keys()))
                     frames_to_convert = list(dict.fromkeys(frames_to_convert))
@@ -51,49 +46,38 @@ def turn_into_images_project(api: sly.Api, task_id, context, state, app_logger):
                 else:
                     frames_to_convert = list(range(0, video_info.frames_count))
 
-                if need_download:
-                    image_paths = f.video_to_images(dataset_name, video_path, frames_to_convert)
-
-                names = []
-                images = []
-                metas = []
-                anns = []
-                progress = sly.Progress("Video: {!r}".format(video_info.name), len(frames_to_convert))
-                for frame_index in frames_to_convert:
-                    names.append('{}_frame_{:05d}.jpg'.format(name, frame_index))
-                    if need_download is False and g.OPTIONS == "annotated":
-                        images.append(api.video.frame.download_np(video_info.id, frame_index))
+                progress = sly.Progress("Processing video frames: {!r}".format(video_info.name), len(frames_to_convert))
+                for batch_frames in sly.batched(frames_to_convert):
+                    metas = []
+                    anns = []
+                    if need_download_video:
+                        images_names, images_paths = f.get_frames_from_video(dataset_name, video_path, batch_frames)
                     else:
-                        im = cv2.imread(image_paths[frame_index])
-                        im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
-                        images.append(im)
+                        images_names, images_paths = f.get_frames_from_api(api, video_info.id, dataset_name, batch_frames)
+                    for frame_index in batch_frames:
+                        metas.append({
+                            "video_id": video_info.id,
+                            "video_name": video_info.name,
+                            "frame_index": frame_index,
+                            "video_dataset_id": video_info.dataset_id,
+                            "video_dataset_name": dataset.name,
+                            "video_project_id": g.project.id,
+                            "video_project_name": g.project.name
+                        })
 
-                    metas.append({
-                        "video_id": video_info.id,
-                        "video_name": video_info.name,
-                        "frame_index": frame_index,
-                        "video_dataset_id": video_info.dataset_id,
-                        "video_dataset_name": dataset.name,
-                        "video_project_id": g.project.id,
-                        "video_project_name": g.project.name
-                    })
+                        labels = []
+                        frame_annotation = ann.frames.get(frame_index)
+                        if frame_annotation is not None:
+                            for figure in frame_annotation.figures:
+                                tags_to_assign = object_props[figure.parent_object.key()].copy()
+                                tags_to_assign.extend(object_frame_tags[figure.parent_object.key()].get(frame_index, []).copy())
+                                cur_label = sly.Label(figure.geometry, figure.parent_object.obj_class,
+                                                      sly.TagCollection(tags_to_assign))
+                                labels.append(cur_label)
 
-                    labels = []
-                    frame_annotation = ann.frames.get(frame_index)
-                    if frame_annotation is not None:
-                        for figure in frame_annotation.figures:
-                            tags_to_assign = object_props[figure.parent_object.key()].copy()
-                            tags_to_assign.extend(object_frame_tags[figure.parent_object.key()].get(frame_index, []).copy())
-                            cur_label = sly.Label(figure.geometry, figure.parent_object.obj_class,
-                                                  sly.TagCollection(tags_to_assign))
-                            labels.append(cur_label)
-
-                    img_tags = video_props.copy() + video_frame_tags.get(frame_index, []).copy()
-                    anns.append(sly.Annotation(ann.img_size, labels=labels, img_tags=sly.TagCollection(img_tags)))
-
-                    if len(names) >= 5:
-                        f.upload_and_reset(api, dst_dataset.id, names, images, anns, metas, progress)
-                f.upload_and_reset(api, dst_dataset.id, names, images, anns, metas, progress)
+                        img_tags = video_props.copy() + video_frame_tags.get(frame_index, []).copy()
+                        anns.append(sly.Annotation(ann.img_size, labels=labels, img_tags=sly.TagCollection(img_tags)))
+                    f.upload_frames(api, dst_dataset.id, images_names, images_paths, anns, metas, progress)
     g.my_app.stop()
 
 
