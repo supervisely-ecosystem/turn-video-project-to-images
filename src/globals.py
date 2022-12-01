fc3e4b23ade85cbe2d31fb86f0ac0e72906fe5fc
cxnt <pavelbartsits@gmail.com>
2022-12-01 17:39:44 +0400

    use original images names from project custom data

--- src/globals.py
+++ src/globals.py
@@ -1,17 +1,22 @@
 import os
-import json
-import supervisely as sly
-from supervisely.io.fs import mkdir
 from distutils.util import strtobool
+
+import supervisely as sly
+from dotenv import load_dotenv
+from supervisely.io.fs import mkdir
+
+if sly.is_development():
+    load_dotenv("local.env")
+    load_dotenv(os.path.expanduser("~/supervisely.env"))
 
 logger = sly.logger
 
 my_app = sly.AppService()
 api: sly.Api = my_app.public_api
 
-TEAM_ID = int(os.environ['context.teamId'])
-WORKSPACE_ID = int(os.environ['context.workspaceId'])
-PROJECT_ID = int(os.environ["modal.state.slyProjectId"])
+TEAM_ID = sly.env.team_id()
+WORKSPACE_ID = sly.env.workspace_id()
+PROJECT_ID = sly.env.project_id()
 
 SAMPLE_RESULT_FRAMES = bool(strtobool(os.getenv("modal.state.sampleResultFrames")))
 if SAMPLE_RESULT_FRAMES:
@@ -19,11 +24,16 @@ if SAMPLE_RESULT_FRAMES:
 
 LOG_LEVEL = str(os.environ["LOG_LEVEL"])
 
-OPTIONS = os.environ['modal.state.Options']
-BATCH_SIZE = int(os.environ['modal.state.batchSize'])
+OPTIONS = os.environ["modal.state.Options"]
+BATCH_SIZE = int(os.environ["modal.state.batchSize"])
 
-SELECTED_DATASETS = json.loads(os.environ["modal.state.selectedDatasets"].replace("'", '"'))
-ALL_DATASETS = os.getenv("modal.state.allDatasets").lower() in ('true', '1', 't')
+# SELECTED_DATASETS = json.loads(
+#     os.environ["modal.state.selectedDatasets"].replace("'", '"')
+# )
+
+SELECTED_DATASETS = None
+
+ALL_DATASETS = os.getenv("modal.state.allDatasets").lower() in ("true", "1", "t")
 if ALL_DATASETS:
     SELECTED_DATASETS = [dataset.name for dataset in api.dataset.get_list(PROJECT_ID)]
 
@@ -40,15 +50,26 @@ project = api.project.get_info_by_id(PROJECT_ID)
 if project is None:
     raise RuntimeError("Project {!r} not found".format(project.name))
 if project.type != str(sly.ProjectType.VIDEOS):
-    raise TypeError("Project type is {!r}, but have to be {!r}".format(project.type, sly.ProjectType.VIDEOS))
+    raise TypeError(
+        "Project type is {!r}, but have to be {!r}".format(
+            project.type, sly.ProjectType.VIDEOS
+        )
+    )
 
 meta_json = api.project.get_meta(project.id)
 meta = sly.ProjectMeta.from_json(meta_json)
 
 if "object_id" not in [tag.name for tag in meta.tag_metas]:
-    vobj_id_tag_meta = sly.TagMeta(name="object_id", value_type=sly.TagValueType.ANY_NUMBER,
-                                   applicable_to=sly.TagApplicableTo.OBJECTS_ONLY)
+    vobj_id_tag_meta = sly.TagMeta(
+        name="object_id",
+        value_type=sly.TagValueType.ANY_NUMBER,
+        applicable_to=sly.TagApplicableTo.OBJECTS_ONLY,
+    )
     meta = meta.add_tag_meta(vobj_id_tag_meta)
 
 if OPTIONS == "annotated" and len(meta.obj_classes) == 0 and len(meta.tag_metas) == 0:
-    raise ValueError("Nothing to convert, there are no tags and classes in project {!r}".format(project.name))
+    raise ValueError(
+        "Nothing to convert, there are no tags and classes in project {!r}".format(
+            project.name
+        )
+    )
--- src/main.py
+++ src/main.py
@@ -1,19 +1,23 @@
 from collections import defaultdict
+from time import time
+
 import supervisely as sly
 from supervisely.video_annotation.key_id_map import KeyIdMap
 
-import globals as g
 import functions as f
-
-from time import time
+import globals as g
 
 
 @g.my_app.callback("turn_into_images_project")
 @sly.timeit
 def turn_into_images_project(api: sly.Api, task_id, context, state, app_logger):
     res_project_name = f"{g.project.name}(images)"
-    dst_project = api.project.create(g.WORKSPACE_ID, res_project_name, type=sly.ProjectType.IMAGES,
-                                     change_name_if_conflict=True)
+    dst_project = api.project.create(
+        g.WORKSPACE_ID,
+        res_project_name,
+        type=sly.ProjectType.IMAGES,
+        change_name_if_conflict=True,
+    )
     api.project.update_meta(dst_project.id, g.meta.to_json())
 
     key_id_map = KeyIdMap()
@@ -26,19 +30,31 @@ def turn_into_images_project(api: sly.Api, task_id, context, state, app_logger):
                 general_time = time()
                 ann_info = api.video.annotation.download(video_info.id)
                 ann = sly.VideoAnnotation.from_json(ann_info, g.meta, key_id_map)
-                if g.OPTIONS == "annotated" and len(ann.tags) == 0 and len(ann.frames) == 0:
-                    g.my_app.logger.warn(f"Video {video_info.name} annotation is empty in Dataset {dataset_name}")
+                if (
+                    g.OPTIONS == "annotated"
+                    and len(ann.tags) == 0
+                    and len(ann.frames) == 0
+                ):
+                    g.my_app.logger.warn(
+                        f"Video {video_info.name} annotation is empty in Dataset {dataset_name}"
+                    )
                     continue
 
                 frames_to_convert = []
                 video_props = []
                 video_frame_tags = defaultdict(list)
-                f.convert_tags(ann.tags, video_props, video_frame_tags, frames_to_convert)
+                f.convert_tags(
+                    ann.tags, video_props, video_frame_tags, frames_to_convert
+                )
                 object_frame_tags = defaultdict(lambda: defaultdict(list))
                 object_props = defaultdict(list)
                 for vobject in ann.objects:
-                    f.convert_tags(vobject.tags, object_props[vobject.key()], object_frame_tags[vobject.key()],
-                                   frames_to_convert)
+                    f.convert_tags(
+                        vobject.tags,
+                        object_props[vobject.key()],
+                        object_frame_tags[vobject.key()],
+                        frames_to_convert,
+                    )
                     vobject_id = key_id_map.get_object_id(vobject.key())
                     f.add_object_id_tag(vobject_id, object_props[vobject.key()])
                 if g.OPTIONS == "annotated":
@@ -50,19 +66,31 @@ def turn_into_images_project(api: sly.Api, task_id, context, state, app_logger):
 
                 if g.SAMPLE_RESULT_FRAMES:
                     if g.OPTIONS == "all":
-                        frames_to_convert = frames_to_convert[::g.FRAMES_STEP]
+                        frames_to_convert = frames_to_convert[:: g.FRAMES_STEP]
                     else:
-                        frames_to_convert = f.calc_frame_step(frames_to_convert=frames_to_convert, frame_step=g.FRAMES_STEP)
+                        frames_to_convert = f.calc_frame_step(
+                            frames_to_convert=frames_to_convert,
+                            frame_step=g.FRAMES_STEP,
+                        )
 
-                progress = sly.Progress("Processing video frames: {!r}".format(video_info.name), len(frames_to_convert))
+                progress = sly.Progress(
+                    "Processing video frames: {!r}".format(video_info.name),
+                    len(frames_to_convert),
+                )
 
                 # total_images_size = 0
-                for batch_index, batch_frames in enumerate(sly.batched(frames_to_convert, batch_size=g.BATCH_SIZE)):
+                for batch_index, batch_frames in enumerate(
+                    sly.batched(frames_to_convert, batch_size=g.BATCH_SIZE)
+                ):
                     metas = []
                     anns = []
                     local_time = time()
-                    images_names, images = f.get_frames_from_api(api, video_info.id, video_info.name, batch_frames)
-                    g.logger.debug(f'extracted {len(batch_frames)} by {time() - local_time} seconds')
+                    images_names, images = f.get_frames_from_api(
+                        api, video_info.id, video_info.name, batch_frames, dataset_name
+                    )
+                    g.logger.debug(
+                        f"extracted {len(batch_frames)} by {time() - local_time} seconds"
+                    )
 
                     """
                     too slow calculations, for extreme debug
@@ -73,40 +101,68 @@ def turn_into_images_project(api: sly.Api, task_id, context, state, app_logger):
                     """
 
                     for frame_index in batch_frames:
-                        metas.append({
-                            "video_id": video_info.id,
-                            "video_name": video_info.name,
-                            "frame_index": frame_index,
-                            "video_dataset_id": video_info.dataset_id,
-                            "video_dataset_name": dataset.name,
-                            "video_project_id": g.project.id,
-                            "video_project_name": g.project.name
-                        })
+                        metas.append(
+                            {
+                                "video_id": video_info.id,
+                                "video_name": video_info.name,
+                                "frame_index": frame_index,
+                                "video_dataset_id": video_info.dataset_id,
+                                "video_dataset_name": dataset.name,
+                                "video_project_id": g.project.id,
+                                "video_project_name": g.project.name,
+                            }
+                        )
 
                         labels = []
                         frame_annotation = ann.frames.get(frame_index)
                         if frame_annotation is not None:
                             for figure in frame_annotation.figures:
-                                tags_to_assign = object_props[figure.parent_object.key()].copy()
+                                tags_to_assign = object_props[
+                                    figure.parent_object.key()
+                                ].copy()
                                 tags_to_assign.extend(
-                                    object_frame_tags[figure.parent_object.key()].get(frame_index, []).copy())
-                                cur_label = sly.Label(figure.geometry, figure.parent_object.obj_class,
-                                                      sly.TagCollection(tags_to_assign))
+                                    object_frame_tags[figure.parent_object.key()]
+                                    .get(frame_index, [])
+                                    .copy()
+                                )
+                                cur_label = sly.Label(
+                                    figure.geometry,
+                                    figure.parent_object.obj_class,
+                                    sly.TagCollection(tags_to_assign),
+                                )
                                 labels.append(cur_label)
 
-                        img_tags = video_props.copy() + video_frame_tags.get(frame_index, []).copy()
-                        anns.append(sly.Annotation(ann.img_size, labels=labels, img_tags=sly.TagCollection(img_tags)))
+                        img_tags = (
+                            video_props.copy()
+                            + video_frame_tags.get(frame_index, []).copy()
+                        )
+                        anns.append(
+                            sly.Annotation(
+                                ann.img_size,
+                                labels=labels,
+                                img_tags=sly.TagCollection(img_tags),
+                            )
+                        )
 
-                    if g.LOG_LEVEL == 'debug':
+                    if g.LOG_LEVEL == "debug":
                         f.distort_frames(images)
-                        g.logger.debug(f'{len(images)} frames distorted')
+                        g.logger.debug(f"{len(images)} frames distorted")
 
-                    f.upload_frames(api, dst_dataset.id, images_names, images, anns, metas,
-                                    f'{batch_index}/{int(len(frames_to_convert) / g.BATCH_SIZE)}')
+                    f.upload_frames(
+                        api,
+                        dst_dataset.id,
+                        images_names,
+                        images,
+                        anns,
+                        metas,
+                        f"{batch_index}/{int(len(frames_to_convert) / g.BATCH_SIZE)}",
+                    )
                     progress.iters_done_report(len(images_names))
 
                 # g.logger.debug(f'total images size for video: {total_images_size} MB')
-                g.logger.info(f'video {video_info.name} converted in {time() - general_time} seconds')
+                g.logger.info(
+                    f"video {video_info.name} converted in {time() - general_time} seconds"
+                )
     g.my_app.stop()
 
 
@@ -117,14 +173,19 @@ def stop(api: sly.Api, task_id, context, state, app_logger):
 
 
 def main():
-    if g.LOG_LEVEL == 'debug':
-        g.logger.debug(f'DEBUG MODE ACTIVATED, we will add 50 random pixels to distort images')
+    if g.LOG_LEVEL == "debug":
+        g.logger.debug(
+            f"DEBUG MODE ACTIVATED, we will add 50 random pixels to distort images"
+        )
 
-    sly.logger.info("Script arguments", extra={
-        "TEAM_ID": g.TEAM_ID,
-        "WORKSPACE_ID": g.WORKSPACE_ID,
-        "PROJECT_ID": g.PROJECT_ID
-    })
+    sly.logger.info(
+        "Script arguments",
+        extra={
+            "TEAM_ID": g.TEAM_ID,
+            "WORKSPACE_ID": g.WORKSPACE_ID,
+            "PROJECT_ID": g.PROJECT_ID,
+        },
+    )
 
     # Run application service
     g.my_app.run(initial_events=[{"command": "turn_into_images_project"}])
